c1990ddd9ad2d758a0739938b1cd01c6048cf812
RXY <ranab@qq.com>
2016-06-01 20:36:27 +0800

    Add Module Work: import doc of module doc of function

---
+++ TestMain.py
@@ -0,0 +1,14 @@
+# coding=utf-8
+
+from tri import triangles
+
+def testMain() :
+    print triangles.__doc__
+    g = triangles()
+    n = 0;
+    while n < 10 :
+        print next(g)
+        n += 1
+
+if __name__ == '__main__' :
+    testMain()
--- tri.py
+++ tri.py
@@ -1,19 +1,19 @@
 # coding=utf-8
 
-def tri():
+'Calculate YangHui Triangles.'
+
+__author__ = 'Hammurabi'
+
+def triangles():
+    'Generator of Yanghui Triangles'
     l = [1]
     while True:
         yield l
         l = [1] + [l[i + 1] + l[i] for i in range(0, len(l) - 1)] + [1]
-        # l.append(0)
-        # l = [l[i-1] + l[i] for i in range(0, len(l))]
-        # l = [0] + l + [0]
-        # l =
 
-
-print range(1, 0)
-g = tri()
-n = 0
-while n < 10:
-    n = n + 1
-    print next(g)
+if __name__ == '__main__' :
+    g = triangles()
+    n = 0
+    while n < 10:
+        n = n + 1
+        print next(g)
